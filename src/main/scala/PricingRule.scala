742ca7d57ef5c4379b7dabc468892b87db1e8cb1
Rui Morais <rui.morais@gmail.com>
2016-05-16 10:33:47 +0100

    Simplify the calculation of regular items

--- src/main/scala/PricingRule.scala
+++ src/main/scala/PricingRule.scala
@@ -8,7 +8,7 @@ case class PricingRule(item: Char, price: Double, promotion: (Int, Double)) {
     val numberOfItems = items.count(_ == item)
     val promotionalPacks = numberOfItems / itemsInPromotion
     val promotionalPackPrice = promotionalPacks * promotionalPrice
-    val regularItems = numberOfItems - promotionalPacks * itemsInPromotion
+    val regularItems = numberOfItems % itemsInPromotion
     regularItems * price + promotionalPackPrice
   }
 }
