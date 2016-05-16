99059099ecf4d3b5ce9dc9f8c66653692116ce80
Rui Morais <rui.morais@gmail.com>
2016-05-16 09:39:14 +0100

    Add test for scanning multiple products a single time

--- src/test/scala/CheckoutSpec.scala
+++ src/test/scala/CheckoutSpec.scala
@@ -26,6 +26,23 @@ class CheckoutSpec extends Specification {
       checkout.scan("A")
       checkout.total() === 100
     }
+
+
+    "calculate the total given multiple products are scanned a single time" in {
+      val rules = Seq(
+        PricingRule("A", 50),
+        PricingRule("B", 30),
+        PricingRule("C", 20),
+        PricingRule("D", 15)
+      )
+      val checkout = new Checkout(rules)
+      checkout.scan("A")
+      checkout.scan("B")
+      checkout.scan("C")
+      checkout.scan("D")
+      checkout.total() === 115
+    }
+
   }
 }
 
